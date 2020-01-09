84a29a40b1f328e6b4152aa606b44b835e217eb4
Gabe Gutierrez <gabe.gutierrez@S05552-MBPR.local>
2020-01-09 10:13:24 -0600

    Adding Spring project

---
+++ src/main/java/com/poc/slalom/kakfapocproducer/KakfaPocProducerApplication.java
@@ -0,0 +1,13 @@
+package com.poc.slalom.kakfapocproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakfaPocProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakfaPocProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/slalom/kakfapocproducer/KakfaPocProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.slalom.kakfapocproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakfaPocProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
